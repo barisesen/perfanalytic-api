448d1e02374731c5633663a13c1e91d03832e063
baris.esen <baris@itsmoneo.com>
2020-01-18 22:02:40 +0300

    client request bug fixed && referer added

--- controllers/metric.js
+++ controllers/metric.js
@@ -1,56 +1,51 @@
+const dayjs = require('dayjs');
 const Metric = require('../models/metric');
 
-module.exports.show = (request, reply) => {
-  return reply.code(200).send(
-    [
-      {
-        ttfb: 123123,
-        fcp: 6463,
-        dom_load: 32783,
-        window_load: 87775,
-        created_at: new Date(),
-      },
-      {
-        ttfb: 326623,
-        fcp: 3112,
-        dom_load: 3233,
-        window_load: 213,
-        created_at: new Date(new Date() - 1000),
-      },
-      {
-        ttfb: 12313,
-        fcp: 4123,
-        dom_load: 1234,
-        window_load: 3124123,
-        created_at: new Date(new Date() - 2000),
-      },
-      {
-        ttfb: 7266,
-        fcp: 23678,
-        dom_load: 171273,
-        window_load: 48382,
-        created_at: new Date(new Date() - 3000),
-      },
-    ],
-  );
+module.exports.show = async (request, reply) => {
+  const lastThirtyMin = dayjs().subtract(30, 'minute').valueOf();
+
+  let query = {
+    created_at: { $gte: lastThirtyMin },
+  };
+
+  if (request.query.start_date && request.query.end_date) {
+    query = {
+      created_at: { $gte: request.query.start_date, $lte: request.query.end_date },
+    };
+  }
+
+  await Metric.find(query, (err, doc) => {
+    if (err) {
+      console.error(err);
+      return reply.code(500).send();
+    }
+
+    return reply.code(200).send(doc);
+  });
 };
 
 module.exports.store = (request, reply) => {
-  console.log(request.headers);
+  console.log(request);
+  let params = request.body;
+  if (typeof request.body === 'string') {
+    params = JSON.parse(request.body);
+  }
+
   const metric = new Metric(
     {
       host: request.hostname,
-      path: '/',
-      ttfb: request.body.ttfb,
-      fcp: request.body.fcp,
-      dom_load: request.body.dom_load,
-      window_load: request.body.window_load,
+      referer: request.headers.referer,
+      ttfb: params.ttfb,
+      fcp: params.fcp,
+      dom_load: params.dom_load,
+      window_load: params.window_load,
+      created_at: dayjs().valueOf(),
     },
   );
 
   metric.save((err) => {
     if (err) {
-      console.log(err);
+      // console.log(err);
     }
   });
   return reply.code(200).send();
--- models/metric.js
+++ models/metric.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const MetricSchema = new mongoose.Schema({
   host: { type: String, unique: false, required: true },
-  path: { type: String, unique: false, required: true },
+  referer: { type: String, unique: false, required: true },
   ttfb: { type: Number, unique: false, required: true },
   fcp: { type: Number, unique: false, required: true },
   dom_load: { type: Number, unique: false, required: true },
   window_load: { type: Number, unique: false, required: true },
+  created_at: { type: Number, unique: false, required: true },
 });
 
 module.exports = mongoose.model('Metric', MetricSchema);
